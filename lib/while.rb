9ba2c0d024501260616ad9f7f04efdb638e943a0
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 15:09:20 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
-	levitation_force = 6
-	while levitation_force < 10
+	Wingardium Leviosa = 6
+	while Wingardium Leviosa < 10
 		puts "Wingardium Leviosa"
 		levitation_force += 1
 	end
